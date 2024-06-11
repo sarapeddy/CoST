4e6ec43c80b8718855bfe2dab099b819c71a76c7
sarapeddy <269362@studenti.unimore.it>
2024-06-11 12:24:21 +0200

    Added sh file to run the training and the evaluation on different seeds

---
+++ ETT_CoST_dlinear.sh
@@ -0,0 +1,8 @@
+for seed in $(seq 0 4); do
+  # multivar
+  python3 -u script.py --dataset ETTh1 --run_name forecast_multivar --alpha 0.0005 --kernels 1 2 4 8 16 32 64 128 --max-train-length 201 --batch-size 128 --archive forecast_csv --repr-dims 320 --max-threads 8 --seed ${seed} --eval
+  python3 -u script.py --dataset ETTh2 --run_name forecast_multivar --alpha 0.0005 --kernels 1 2 4 8 16 32 64 128 --max-train-length 201 --batch-size 128 --archive forecast_csv --repr-dims 320 --max-threads 8 --seed ${seed} --eval
+  python3 -u script.py --dataset ETTm1 --run_name forecast_multivar --alpha 0.0005 --kernels 1 2 4 8 16 32 64 128 --max-train-length 201 --batch-size 128 --archive forecast_csv --repr-dims 320 --max-threads 8 --seed ${seed} --eval
+  python3 -u script.py --dataset ETTm2 --run_name forecast_multivar --alpha 0.0005 --kernels 1 2 4 8 16 32 64 128 --max-train-length 201 --batch-size 128 --archive forecast_csv --repr-dims 320 --max-threads 8 --seed ${seed} --eval
+
+done
