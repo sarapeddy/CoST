984818d8cfa4a811689b4f9734e1a876c6ea2814
sarapeddy <269362@studenti.unimore.it>
2024-07-07 11:30:23 +0200

    Deleted unusefull file

--- script_sh_electricity.sh
+++
@@ -1,9 +0,0 @@
-
-for seed in $(seq 0 4); do
-
-  # multivar Cost
-  python -u train.py --dataset electricity --run_name forecast_multivar --alpha 0.0005 --kernels 1 2 4 8 16 32 64 128 --max-train-length 201 --batch-size 128 --archive forecast_csv --repr-dims 320 --max-threads 8 --seed ${seed} --eval
-  # multivar CostDlinear
-  python -u script.py --dataset electricity --run_name forecast_multivar --alpha 0.0005 --kernels 1 2 4 8 16 32 64 128 --max-train-length 201 --batch-size 128 --archive forecast_csv --repr-dims 320 --max-threads 8 --seed ${seed} --eval
-
-done
